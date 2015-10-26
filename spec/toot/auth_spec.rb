1c6308675cfc91ac6b28e4b100705d45a53ab6da
Travis Petticrew <travis@petticrew.net>
2015-10-26 10:25:01 -0500

    Configure RSpec

--- spec/toot/auth_spec.rb
+++ spec/toot/auth_spec.rb
@@ -1,11 +1,7 @@
 require 'spec_helper'
 
-describe Toot::Auth do
+RSpec.describe Toot::Auth do
   it 'has a version number' do
     expect(Toot::Auth::VERSION).not_to be nil
-  end
-
-  it 'does something useful' do
-    expect(false).to eq(true)
   end
 end
